450138261f1ed985dfffe9e035f7feda583a4f54
corbinSanders <corbin@cos.io>
2019-11-26 17:01:43 -0500

    FIxing more flake stuff

--- IA/tests/test_IA_consume_files.py
+++ IA/tests/test_IA_consume_files.py
@@ -53,17 +53,8 @@ class TestIAFiles(unittest.TestCase):
         with mock.patch('builtins.open', mock.mock_open()) as m:
             consume_files('jj81a', None, 'tests/test_folder')
             mock_mkdir.assert_called_with(os.path.join(HERE, 'test_folder/jj81a/files'))
-<<<<<<< HEAD
-<<<<<<< HEAD
             zip_path = os.path.join(HERE, 'test_folder/jj81a/files/jj81a.zip')
             m.assert_called_with(zip_path, 'wb')
             mock_zipfile.assert_called_with(zip_path, 'r')
-=======
             m.assert_called_with(os.path.join(HERE, 'test_folder/jj81a/files/jj81a.zip'), 'wb')
-=======
-            zip_path = os.path.join(HERE, 'test_folder/jj81a/files/jj81a.zip')
-            m.assert_called_with(zip_path, 'wb')
->>>>>>> Fixing some flake errors
-            mock_zipfile.assert_called_with(os.path.join(HERE, 'test_folder/jj81a/files/jj81a.zip'), 'r')
->>>>>>> Fixing tests more
-            mock_rm.assert_called_with(os.path.join(HERE, 'test_folder/jj81a/files/jj81a.zip'))
+
